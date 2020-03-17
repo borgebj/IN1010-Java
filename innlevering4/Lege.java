0912de6745fe87a374c4aa8466a72f3ce44930d6
borgebj <57920815+borgebj@users.noreply.github.com>
2020-03-17 14:46:23 +0100

    Update Lege.java

--- innlevering4/Lege.java
+++ innlevering4/Lege.java
@@ -1,6 +1,6 @@
 //TODO: Del D: Klassen lege 
 
-public class Lege {
+public class Lege implements Comparable<Lege> {
 
     // instansvariabel legeNavn fra parameter for navnet til legen
     String legeNavn;
@@ -17,5 +17,12 @@ public class Lege {
     @Override // metode som overskriver toString()-metoden og returner relevant info
     public String toString() {
         return ("Legenavn: " + legeNavn);
+    }
+    
+    /* NYTT ! */ 
+    
+    @Override // overskriving av "compareTo" for "Lege"
+    public compareTo(Lege lege) {
+        // IDK wtf skal være her oppgaven er dårlig
     }
 }
