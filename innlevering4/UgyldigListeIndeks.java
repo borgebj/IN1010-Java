55ca2461ab7fc9a6c5073da93df065adf6ed89b6
borgebj <57920815+borgebj@users.noreply.github.com>
2020-03-18 18:58:56 +0100

    Update UgyldigListeIndeks.java
    Gjorde UgyldigListeIndeks public

--- innlevering4/UgyldigListeIndeks.java
+++ innlevering4/UgyldigListeIndeks.java
@@ -1,5 +1,5 @@
 
-class UgyldigListeIndeks extends RuntimeException {
+public class UgyldigListeIndeks extends RuntimeException {
     UgyldigListeIndeks(int indeks){
         super("Ugyldig indeks:" + indeks);
     }
