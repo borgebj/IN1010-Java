e123a40264c26b6a3a0811c203121197af229b3d
borgebj <57920815+borgebj@users.noreply.github.com>
2020-03-18 19:14:33 +0100

    test
    Test

--- innlevering4/Legesystem.java
+++ innlevering4/Legesystem.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Legesystem {
+
+    public Legesystem (String fil) throws FileNotFoundException {
+
+        Scanner minFil = new Scanner(new File(fil));
+
+        while (minFil.hasNextLine()) {
+            System.out.println(minFil.nextLine());
+        }
+    }
 
 }
