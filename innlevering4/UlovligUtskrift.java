5913c84333f0bc4a91730afd35830d435a6c6a0b
borgebj <57920815+borgebj@users.noreply.github.com>
2020-03-25 15:12:18 +0100

    Update UlovligUtskrift.java
    Fjerent noe tull, mb

--- innlevering4/UlovligUtskrift.java
+++ innlevering4/UlovligUtskrift.java
@@ -4,38 +4,3 @@ public class UlovligUtskrift extends Exception{
         super(lege + " har ikke lov til å skrive ut " + legemiddel.hentNavn());
     }
 }
-
-
-/*
-                    // LEGE-delen
-                    delay(500);
-                    System.out.print("Hva er lege navn? \n > ");
-                    String legeNavn = scanner.nextLine().trim().toLowerCase();
-                    for (Lege b : leger) {
-                        if (legeNavn.equals(b.hentNavn().trim().toLowerCase())) {
-
-                            // sjekker om lege er vanlig eller spesialist
-                            if (b instanceof Spesialist) {
-                                System.out.print("Hva er Kontroll-ID? \n > ");
-                                int kontrollID = scanner.nextInt();
-                                scanner.nextLine();
-
-                                // vi vet at lege = spesialist, saa vi caster lege til spesialist for aa bruke "hentKontrollID()"
-                                Spesialist spesialist = (Spesialist) b;
-
-                                if (kontrollID == spesialist.hentKontrollID()) {
-                                    legeMatch = true;
-                                    lege = b;
-                                    break;
-                                }
-                            }
-                            else if (b instanceof Lege) {
-                                if ( !(legemiddel instanceof Narkotisk)) {
-                                    legeMatch = true;
-                                    lege = b;
-                                    break;
-                                } else { narkoMatch = true; }
-                            }
-                        }
-                    }
- */
