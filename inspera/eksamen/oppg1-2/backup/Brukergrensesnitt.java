7f8746a462fa193087ec094c2cd2ae5c1d2409de
borgebj <57920815+borgebj@users.noreply.github.com>
2020-06-08 20:22:00 +0200

    Skattejakt - FERDIG -
    Fullstendig ferdig med skattejakt spill fra eksamen 2020 in1010

--- inspera/eksamen/oppg1-2/backup/Brukergrensesnitt.java
+++
@@ -1,6 +0,0 @@
-
-public interface Brukergrensesnitt {
-    void giStatus();
-    int beOmKommando(String spoersmaal, String[] alternativer);
-    void sleep(long t); //egen-metode for aa skape korte stopp i programmet
-}
