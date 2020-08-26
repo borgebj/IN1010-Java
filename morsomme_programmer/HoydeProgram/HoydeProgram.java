c1aca98d632275b1baaa468db1718e17cf4cf3b8
Børge Bjørnstadjordet <57920815+borgebj@users.noreply.github.com>
2020-08-26 16:11:15 +0200

    Endringer

--- morsomme_programmer/HoydeProgram/HoydeProgram.java
+++ morsomme_programmer/HoydeProgram/HoydeProgram.java
@@ -3,6 +3,8 @@ import javafx.application.Platform;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -68,6 +70,10 @@ public class HoydeProgram extends Application {
     class BeregnBehandler implements EventHandler<ActionEvent> {
         @Override
         public void handle(ActionEvent e) {
+            Image image = new Image("stonks.jpg", 125, 125, true, true);
+            ImageView imageView = new ImageView(image);
+            alert.setGraphic(imageView);
+
             String hoyde = prompt.getText();
             alert.setTitle("Resultat");
             alert.setHeaderText("Hoyden din er " + hoyde + "cm!");
