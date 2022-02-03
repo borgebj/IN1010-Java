6cab4f846cb92cf2c7748e1ca97d16e3daf31408
Børge Bjørnstadjordet <57920815+borgebj@users.noreply.github.com>
2022-02-03 18:14:55 +0100

    Delete innlevering0 directory

--- innlevering0/oppgave1/Bil1.java
+++
@@ -1,9 +0,0 @@
-
-// public klasse Bil1 som kan bli brukt av andre filer
-public class Bil1 {
-
-    // public metode skriv som printer ut til terminalen tekst
-   public void skriv() {
-       System.out.println("Jeg er en bil");
-   }
-}
